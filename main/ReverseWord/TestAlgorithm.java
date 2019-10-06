2666e5d6c2b9798ec359837b3b41aa2b93b2790b
胡羽凡 <1790199849@qq.com>
2019-10-06 23:33:36 +0800

    反转字符串单词-main

---
+++ main/ReverseWord/TestAlgorithm.java
@@ -0,0 +1,27 @@
+package AlgorithmPackge;
+
+import java.util.Arrays;
+
+/**
+ * @ClassName TestAlgorithm
+ * @Description: TODO 这是所有方法的入口
+ * @Author 余霜
+ * @Version V1.0
+ **/
+
+public class TestAlgorithm {
+
+    public static void main(String[] args) {
+
+        String s = "hello,world,,mirror,,apple,incisive,include,contain";
+
+
+        String[] a = s.split(",");
+
+        ReverseWord ys = new ReverseWord();
+        String[] res = ys.reverseWord(s);
+
+        System.out.println(Arrays.toString(res));
+
+    }
+}
