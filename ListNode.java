4566c1c5feb0df0dd8ad2021580eaeca78bbc3ec
H-Always <1790199849@qq.com>
2020-02-06 20:16:37 +0800

    链表文件

---
+++ ListNode.java
@@ -0,0 +1,16 @@
+/**
+ * @ClassName: ListNode
+ * @Description: 链表文件
+ * @Author: 余霜
+ * @date: 2020/2/6/17:43
+ * @Version: 1.0
+ */
+//Definition for singly-linked list.
+public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+}
