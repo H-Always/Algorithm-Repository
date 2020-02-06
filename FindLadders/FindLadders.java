b97c49bda38bf561038dd8ba74dfc110b99b69d0
H-Always <1790199849@qq.com>
2020-02-06 19:43:07 +0800

    单词接龙 II

---
+++ FindLadders/FindLadders.java
@@ -0,0 +1,173 @@
+import java.util.*;
+/**
+ * @ClassName FindLadders
+ * @Description: 单词接龙 II
+ * @Author LeetCode
+ * @Date 2019/12/4 16:23
+ * @Version V1.0
+ **/
+
+/**
+ *给定两个单词（beginWord 和 endWord）和一个字典 wordList，找出所有从 beginWord 到 endWord 的最短转换序列。转换需遵循如下规则：
+ *
+ * 每次转换只能改变一个字母。
+ * 转换过程中的中间单词必须是字典中的单词。
+ * 说明:
+ *
+ * 如果不存在这样的转换序列，返回一个空列表。
+ * 所有单词具有相同的长度。
+ * 所有单词只由小写字母组成。
+ * 字典中不存在重复的单词。
+ * 你可以假设 beginWord 和 endWord 是非空的，且二者不相同。
+ */
+
+public class FindLadders {
+    public List<List<String>> findLadders_DFS(String beginWord, String endWord, List<String> wordList) {
+        // 结果集
+        List<List<String>> result = new ArrayList<>();
+        Set<String> distSet = new HashSet<>(wordList);
+
+        if (!distSet.contains(endWord)) {
+            return result;
+        }
+        // 已经访问过的单词集合：只找最短路径，所以之前出现过的单词不用出现在下一层
+        Set<String> visited = new HashSet<>();
+        // 累积每一层的结果队列
+        Queue<List<String>> queue= new LinkedList<>();
+        List<String> list = new ArrayList<>(Collections.singletonList(beginWord));
+        queue.add(list);
+        visited.add(beginWord);
+        // 是否到达符合条件的层
+        boolean flag = false;
+        while (!queue.isEmpty() && !flag) {
+            // 上一层的结果队列
+            int size = queue.size();
+            // 该层添加的所有元素：每层必须在所有结果都添加完新的单词之后，再将这些单词统一添加到已使用单词集合
+            // 如果直接添加到 visited 中，会导致该层本次结果添加之后的相同添加行为失败
+            // 如：该层遇到目标单词，有两条路径都可以遇到，但是先到达的将该单词添加进 visited 中，会导致第二条路径无法添加
+            Set<String> subVisited = new HashSet<>();
+            for (int i = 0; i < size; i++) {
+                List<String> path = queue.poll();
+                // 获取该路径上一层的单词
+                assert path != null;
+                String word = path.get(path.size() - 1);
+                char[] chars = word.toCharArray();
+                // 寻找该单词的下一个符合条件的单词
+                for (int j = 0; j < chars.length; j++) {
+                    char temp = chars[j];
+                    for (char ch = 'a'; ch <= 'z'; ch++) {
+                        chars[j] = ch;
+                        if (temp == ch) {
+                            continue;
+                        }
+                        String str = new String(chars);
+                        // 符合条件：在 wordList 中 && 之前的层没有使用过
+                        if (distSet.contains(str) && !visited.contains(str)) {
+                            // 生成新的路径
+                            List<String> pathList = new ArrayList<>(path);
+                            pathList.add(str);
+                            // 如果该单词是目标单词：将该路径添加到结果集中，查询截止到该层
+                            if (str.equals(endWord)) {
+                                flag = true;
+                                result.add(pathList);
+                            }
+                            // 将该路径添加到该层队列中
+                            queue.add(pathList);
+                            // 将该单词添加到该层已访问的单词集合中
+                            subVisited.add(str);
+                        }
+                    }
+                    chars[j] = temp;
+                }
+            }
+            // 将该层所有访问的单词添加到总的已访问集合中
+            visited.addAll(subVisited);
+        }
+        return result;
+    }
+
+ //双向DFS+DFS
+    public List<List<String>> findLadders_BFS_DFS(String beginWord, String endWord, List<String> wordList) {
+        // 结果集
+        List<List<String>> res = new ArrayList<>();
+        Set<String> words = new HashSet<>(wordList);
+        // 字典中不包含目标单词
+        if (!words.contains(endWord)) {
+            return res;
+        }
+        // 存放关系：每个单词可达的下层单词
+        Map<String, List<String>> mapTree = new HashMap<>();
+        Set<String> begin = new HashSet<>(), end = new HashSet<>();
+        begin.add(beginWord);
+        end.add(endWord);
+        if (buildTree(words, begin, end, mapTree, true)) {
+            dfs(res, mapTree, beginWord, endWord, new LinkedList<>());
+        }
+        return res;
+    }
+
+    // 双向BFS，构建每个单词的层级对应关系
+    private boolean buildTree(Set<String> words, Set<String> begin, Set<String> end, Map<String, List<String>> mapTree, boolean isFront){
+        if (begin.size() == 0) {
+            return false;
+        }
+        // 始终以少的进行探索
+        if (begin.size() > end.size()) {
+            return buildTree(words, end, begin, mapTree, !isFront);
+        }
+        // 在已访问的单词集合中去除
+        words.removeAll(begin);
+        // 标记本层是否已到达目标单词
+        boolean isMeet = false;
+        // 记录本层所访问的单词
+        Set<String> nextLevel = new HashSet<>();
+        for (String word : begin) {
+            char[] chars = word.toCharArray();
+            for (int i = 0; i < chars.length; i++) {
+                char temp = chars[i];
+                for (char ch = 'a'; ch <= 'z'; ch++) {
+                    chars[i] = ch;
+                    String str = String.valueOf(chars);
+                    if (words.contains(str)) {
+                        nextLevel.add(str);
+                        // 根据访问顺序，添加层级对应关系：始终保持从上层到下层的存储存储关系
+                        // true: 从上往下探索：word -> str
+                        // false: 从下往上探索：str -> word（查找到的 str 是 word 上层的单词）
+                        String key = isFront ? word : str;
+                        String nextWord = isFront ? str : word;
+                        // 判断是否遇见目标单词
+                        if (end.contains(str)) {
+                            isMeet = true;
+                        }
+                        if (!mapTree.containsKey(key)) {
+                            mapTree.put(key, new ArrayList<>());
+                        }
+                        mapTree.get(key).add(nextWord);
+                    }
+                }
+                chars[i] = temp;
+            }
+        }
+        if (isMeet) {
+            return true;
+        }
+        return buildTree(words, nextLevel, end, mapTree, isFront);
+    }
+
+    // DFS: 组合路径
+    private void dfs (List<List<String>> res, Map<String, List<String>> mapTree, String beginWord, String endWord, LinkedList<String> list) {
+        list.add(beginWord);
+        if (beginWord.equals(endWord)) {
+            res.add(new ArrayList<>(list));
+            list.removeLast();
+            return;
+        }
+        if (mapTree.containsKey(beginWord)) {
+            for (String word : mapTree.get(beginWord)) {
+                dfs(res, mapTree, word, endWord, list);
+            }
+        }
+        list.removeLast();
+    }
+
+}
