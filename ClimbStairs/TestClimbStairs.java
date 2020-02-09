d60a925b4e481352ebd4292a9e9978a58e5d6396
H-Always <1790199849@qq.com>
2020-02-09 15:20:05 +0800

    跳台阶测试

---
+++ ClimbStairs/TestClimbStairs.java
@@ -0,0 +1,14 @@
+/**
+ * @ClassName: TestClimbStairs
+ * @Description: 跳台阶测试
+ * @Author: 余霜
+ * @date: 2020/2/9/15:00
+ * @Version: 1.0
+ */
+public class TestClimbStairs {
+    public static void main(String[] args) {
+        ClimbStairs ys = new ClimbStairs();
+
+        System.out.println(ys.climbStair(40));
+    }
+}
