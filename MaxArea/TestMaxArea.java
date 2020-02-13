1dbd48bd95d44ebd71d8c2066aa1edb3866d60aa
H-Always <1790199849@qq.com>
2020-02-13 22:01:59 +0800

    盛水最多的容器测试

---
+++ MaxArea/TestMaxArea.java
@@ -0,0 +1,17 @@
+/**
+ * @ClassName: TestMaxArea
+ * @Description: 盛水最多的容器测试
+ * @Author: 余霜
+ * @date: 2020/2/12/17:59
+ * @Version: 1.0
+ */
+public class TestMaxArea {
+    public static void main(String[] args) {
+
+        int[] a = {1,8,6,2,5,4,8,3,7};
+
+        MaxArea ys = new MaxArea();
+        System.out.println(ys.maxArea(a));
+    }
+
+}
