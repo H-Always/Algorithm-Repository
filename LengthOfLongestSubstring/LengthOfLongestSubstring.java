ef084cea2818b83730f90784e8d13fcc9317f7b6
H-Always <1790199849@qq.com>
2020-02-25 19:07:38 +0800

    无重复字符的最长子串

--- LengthOfLongestSubstring/LengthOfLongestSubstring.java
+++ LengthOfLongestSubstring/LengthOfLongestSubstring.java
@@ -34,7 +34,7 @@ import java.util.Map;
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 
-
+//双循环暴力解，没什么好说的，想写的简单一点转化成为数组也行
 public class LengthOfLongestSubstring {
     public int lengthOfLongestSubstring_BF(String s) {
         if (s.length() == 0)
@@ -60,6 +60,12 @@ public class LengthOfLongestSubstring {
     }
 
 
+    //看到这个题立刻想到哈希函数，没
+    //这个整体的思路是：设置两个指针begin和end，以及一个哈希表用存放每一个元素的最后一次出现的位置
+    //首先第一个字母一定不是重复，所以每一次循环都是end往后跑
+    //判断当前end是否和前面所有字符重复，没有重复就算一下当前begin和end的差和当前max比较选择大的一个
+    //当判断重复，就把当前重复的下一个位置(这里的下一个位置就是最大不重复的下标)复制给begin，然后计算最大长度
+    //最后要把当前字符最后一次出现的位置添加到哈希表里
     public int lengthOfLongestSubstring(String s) {
         int n = s.length(), max = 0;
         //建立一个map函数，用O(1)的时间复杂度来判断重复
@@ -67,14 +73,14 @@ public class LengthOfLongestSubstring {
 
         for (int begin = 0, end = 0; end < n; end++) {
             char temp = s.charAt(end);
-            //判断是否重复，如果不重复就添加
+            //判断是否重复，如果重复就更新begin的位置
             if (map.containsKey(temp)) {
-                begin = Math.max(map.get(temp), begin);
+                begin = Math.max(map.get(temp)+1, begin);
             }
             //max = Math.max(max, end - begin + 1);
             max = Math.max((end - begin + 1), max);
-            //key:字母   value:不重复的下标，这里不重复是为了寻找begin的位置
-            map.put(s.charAt(end), end + 1);
+            //key:字母   value:更新当前遍历元素下标，同时也是为了寻找begin
+            map.put(s.charAt(end), end);
         }
         return max;
     }
