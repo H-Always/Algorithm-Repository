08067fbbdb4cbcd34821753430eb21bea47dc321
H-Always <1790199849@qq.com>
2020-03-06 21:25:57 +0800

    算法测试

---
+++ IntToRoman/TestIntToRoman.java
@@ -0,0 +1,16 @@
+/**
+ * @ClassName: TestIntToRoman
+ * @Description: 算法测试
+ * @Author: 余霜
+ * @date: 2020/3/6/18:56
+ * @Version: 1.0
+ */
+public class TestIntToRoman {
+    public static void main(String[] args) {
+
+        IntToRoman ys = new IntToRoman();
+
+        System.out.println(ys.intToRoman_Greedy(2374));
+
+    }
+}
