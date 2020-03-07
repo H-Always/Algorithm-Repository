0efb2ec7b6a55533ad88bc288de55b3a79aad5f7
H-Always <1790199849@qq.com>
2020-03-07 22:40:40 +0800

    算法测试

---
+++ LetterCombinations/TestLetterCombinations.java
@@ -0,0 +1,15 @@
+/**
+ * @ClassName: TestLetterCombinations
+ * @Description: 算法测试
+ * @Author: 余霜
+ * @date: 2020/3/7/11:27
+ * @Version: 1.0
+ */
+public class TestLetterCombinations {
+    public static void main(String[] args) {
+        LetterCombinations ys = new LetterCombinations();
+
+        System.out.println(ys.letterCombinations("34565"));
+
+    }
+}
