d83ac5e1031581ee98666ff1a2349bf9980fc95f
H-Always <1790199849@qq.com>
2020-03-10 12:30:42 +0800

    树结构

---
+++ DataStructure/TreeNode.java
@@ -0,0 +1,19 @@
+/**
+ * @ClassName: TreeNode
+ * @Description: 二叉树结构
+ * @Author: 余霜
+ * @Date: 2020/03/10 08:44
+ * @Version: V1.0
+ **/
+
+//Definition for a binary tree node.
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
