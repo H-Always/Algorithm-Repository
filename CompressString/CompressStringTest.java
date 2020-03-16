aab5a3c9ca7516305ea646ee27d7edfb6980e97d
H-Always <1790199849@qq.com>
2020-03-16 08:53:29 +0800

    算法测试

---
+++ CompressString/CompressStringTest.java
@@ -0,0 +1,19 @@
+/**
+ * @ClassName: CompressStringTest
+ * @Description: 算法测试
+ * @Author: 余霜
+ * @Date: 2020/03/16 07:38
+ * @Version: V1.0
+ **/
+public class CompressStringTest {
+    public static void main(String[] args) {
+        CompressString ys = new CompressString();
+
+        String s = "aaaaaaaaaaaa";
+
+        System.out.println(s);
+
+        System.out.println(ys.compressString(s));
+
+    }
+}
