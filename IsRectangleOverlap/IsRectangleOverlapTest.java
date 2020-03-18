633d2ec643d49b68f0298841b84284e5feac7efa
H-Always <1790199849@qq.com>
2020-03-18 23:38:02 +0800

    算法测试

---
+++ IsRectangleOverlap/IsRectangleOverlapTest.java
@@ -0,0 +1,14 @@
+/**
+ * @ClassName: IsRectangleOverlapTest
+ * @Description: 算法测试
+ * @Author: 余霜
+ * @Date: 2020/03/18 18:29
+ * @Version: V1.0
+ **/
+public class IsRectangleOverlapTest {
+    public static void main(String[] args) {
+        IsRectangleOverlap ys = new IsRectangleOverlap();
+
+        ys.isRectangleOverlap(new int[]{0,0,2,2}, new int[]{1,1,3,3});
+    }
+}
