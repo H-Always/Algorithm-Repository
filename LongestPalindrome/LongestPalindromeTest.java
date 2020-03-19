50995fab2cfccbd267bbcbd765ccd2891cc7b86d
H-Always <1790199849@qq.com>
2020-03-19 23:31:16 +0800

    算法测试

---
+++ LongestPalindrome/LongestPalindromeTest.java
@@ -0,0 +1,17 @@
+/**
+ * @ClassName: LongestPalindromeTest
+ * @Description: 算法测试
+ * @Author: 余霜
+ * @Date: 2020/03/19 22:25
+ * @Version: V1.0
+ **/
+public class LongestPalindromeTest {
+    public static void main(String[] args) {
+
+        int a = 'A';
+        LongestPalindrome ys = new LongestPalindrome();
+
+        System.out.println(ys.longestPalindrome("abccccdafsADFFVdghdsfdgVCASDGVArhgegdfd"));
+
+    }
+}
