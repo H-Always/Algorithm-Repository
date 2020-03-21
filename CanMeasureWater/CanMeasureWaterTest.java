676499d375159fb8b7182c0d642354edf963554e
H-Always <1790199849@qq.com>
2020-03-21 10:44:51 +0800

    算法测试

---
+++ CanMeasureWater/CanMeasureWaterTest.java
@@ -0,0 +1,18 @@
+/**
+ * @ClassName: CanMeasureWaterTest
+ * @Description: 算法测试
+ * @Author: 余霜
+ * @Date: 2020/03/21 08:10
+ * @Version: V1.0
+ **/
+public class CanMeasureWaterTest {
+    public static void main(String[] args) {
+        CanMeasureWater ys = new CanMeasureWater();
+
+        System.out.println(ys.canMeasureWater(3,7,45));
+
+
+    }
+
+
+}
