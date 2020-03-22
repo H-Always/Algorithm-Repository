ce93416a31c33f1b5bb9e9cdbd263c8abbbe06a9
H-Always <1790199849@qq.com>
2020-03-22 20:45:16 +0800

    算法测试

---
+++ MinIncrementForUnique/MinIncrementForUniqueTest.java
@@ -0,0 +1,18 @@
+/**
+ * @ClassName: MinIncrementForUniqueTest
+ * @Description: 算法测试
+ * @Author: 余霜
+ * @Date: 2020/03/22 10:50
+ * @Version: V1.0
+ **/
+public class MinIncrementForUniqueTest {
+    public static void main(String[] args) {
+
+        MinIncrementForUnique ys = new MinIncrementForUnique();
+
+            int[] A = {11,2,43,5,4,32,3,4,235,312,23,4,34,32,1,3,312,1,54,5,43,123,324,12,12,12,3,4,12};
+
+        System.out.println(ys.minIncrementForUnique(A));
+
+    }
+}
