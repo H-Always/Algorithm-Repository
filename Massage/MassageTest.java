2455a89a3af4bb83de9c47a6b0cea6b5110c10c8
H-Always <1790199849@qq.com>
2020-03-24 18:02:57 +0800

    算法测试

---
+++ Massage/MassageTest.java
@@ -0,0 +1,17 @@
+/**
+ * @ClassName: MassageTest
+ * @Description:
+ * @Author: 余霜
+ * @Date: 2020/03/24 15:49
+ * @Version: V1.0
+ **/
+public class MassageTest {
+    public static void main(String[] args) {
+
+
+        int[] nums = {1,2,3,11,2,3,4,45,6,154,42,6,34,64,34,4,23,42,2,4,21,23,45};
+
+        Massage ys = new Massage();
+        System.out.println(ys.massage(nums));
+    }
+}
