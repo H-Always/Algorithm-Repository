e17935eacab6d2061c41225e01e5c6d282c6b399
H-Always <1790199849@qq.com>
2020-03-27 10:07:43 +0800

    算法测试

---
+++ HasGroupsSizeX/HasGroupsSizeXTest.java
@@ -0,0 +1,16 @@
+/**
+ * @ClassName: HasGroupsSizeXTest
+ * @Description: 算法测试
+ * @Author: 余霜
+ * @Date: 2020/03/27 09:53
+ * @Version: V1.0
+ **/
+public class HasGroupsSizeXTest {
+    public static void main(String[] args) {
+        HasGroupsSizeX ys = new HasGroupsSizeX();
+
+        int[] desk = {1,2,1,1,1,2,3,3,1,1,4,4};
+
+        System.out.println(ys.hasGroupsSizeX(desk));
+    }
+}
