829a98a1038b872bf4725bdffd2eb30702607672
H-Always <1790199849@qq.com>
2020-03-28 20:41:18 +0800

    算法测试

---
+++ MinimumLengthEncoding/MinimumLengthEncodingTest.java
@@ -0,0 +1,19 @@
+/**
+ * @ClassName: MinimumLengthEncodingTest
+ * @Description: 算法测试
+ * @Author: 余霜
+ * @Date: 2020/03/28 19:15
+ * @Version: V1.0
+ **/
+public class MinimumLengthEncodingTest {
+    public static void main(String[] args) {
+
+      String[] word = {"time", "me", "bell","string","baskba","bet","bat","oracle","mysql","sql","tql"};
+
+        MinimumLengthEncoding ys = new MinimumLengthEncoding();
+
+        System.out.println(ys.minimumLengthEncoding(word));
+
+
+    }
+}
