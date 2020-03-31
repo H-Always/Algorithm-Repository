526969d27361b84b8f620ce10d62742ff104e05a
H-Always <1790199849@qq.com>
2020-03-31 09:07:18 +0800

    算法测试

---
+++ LastRemaining/LastRemainingTest.java
@@ -0,0 +1,15 @@
+/**
+ * @ClassName: LastRemainingTest
+ * @Description: 算法测试
+ * @Author: 余霜
+ * @Date: 2020/03/30 22:18
+ * @Version: V1.0
+ **/
+public class LastRemainingTest {
+    public static void main(String[] args) {
+
+        LastRemaining ys = new LastRemaining();
+
+        ys.lastRemaining(8,3);
+    }
+}
