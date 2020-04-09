6bb5fe82c79377cfeba1b4f00dfbfc47eb6698aa
H-Always <1790199849@qq.com>
2020-04-09 22:51:56 +0800

    算法测试

---
+++ SimpleAlgorithm/GenerateParenthesis/GenerateParenthesisTest.java
@@ -0,0 +1,15 @@
+/**
+ * @ClassName: GenerateParenthesisTest
+ * @Description: 算法测试
+ * @Author: 余霜
+ * @Date: 2020/04/09 22:22
+ * @Version: V1.0
+ **/
+public class GenerateParenthesisTest {
+    public static void main(String[] args) {
+        GenerateParenthesis ys = new GenerateParenthesis();
+
+        ys.generateParenthesis(3);
+
+    }
+}
