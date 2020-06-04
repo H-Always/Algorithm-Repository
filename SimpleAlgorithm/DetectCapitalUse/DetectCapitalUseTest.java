0cfad8b4a0455b5aa8145e79ed147c0ac6c7d4da
H-Always <1790199849@qq.com>
2020-06-04 23:18:38 +0800

    检测大写字母测试

---
+++ SimpleAlgorithm/DetectCapitalUse/DetectCapitalUseTest.java
@@ -0,0 +1,28 @@
+/**
+ * @ClassName: DetectCapitalUseTest
+ * @Description: 检测大写字母测试
+ * @Author: 余霜
+ * @Date: 2020/05/21 23:13
+ * @Version: V1.0
+ **/
+public class DetectCapitalUseTest {
+    public static void main(String[] args) {
+        DetectCapitalUse ys = new DetectCapitalUse();
+
+        String a = "aBadF";
+        String b = "BadF";
+        String c = "Badf";
+        String d = "ABCFE";
+        String e ="abeebq";
+
+       /* System.out.println(ys.detectCapitalUse(a));
+        System.out.println(ys.detectCapitalUse(b));
+        System.out.println(ys.detectCapitalUse(c));
+        System.out.println(ys.detectCapitalUse(d));
+        System.out.println(ys.detectCapitalUse(e));*/
+
+
+
+
+    }
+}
