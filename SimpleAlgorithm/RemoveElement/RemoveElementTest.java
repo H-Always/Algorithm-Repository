ad07f0b9c2de5ecbc848ece78b8c51a69d87dd87
H-Always <1790199849@qq.com>
2020-06-04 23:21:11 +0800

    算法测试

---
+++ SimpleAlgorithm/RemoveElement/RemoveElementTest.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @ClassName: RemoveElementTet
+ * @Description: 移除元素
+ * @Author: 余霜
+ * @Date: 2020/05/20 15:47
+ * @Version: V1.0
+ **/
+public class RemoveElementTest {
+    public static void main(String[] args) {
+
+        RemoveElement ys = new RemoveElement();
+
+        int[] arr = {1,2,3};
+
+        ys.removeElement(arr,3);
+
+
+    }
+
+}
