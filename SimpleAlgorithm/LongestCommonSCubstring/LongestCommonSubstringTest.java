2a542f4806c9d1c1bdda0851c9236472ec56b979
H-Always <1790199849@qq.com>
2020-06-04 23:20:00 +0800

    算法测试

---
+++ SimpleAlgorithm/LongestCommonSCubstring/LongestCommonSubstringTest.java
@@ -0,0 +1,15 @@
+/**
+ * @ClassName: LongestCommonSubstringTest
+ * @Description: 算法测试
+ * @Author: 余霜
+ * @Date: 2020/05/22 10:40
+ * @Version: V1.0
+ **/
+public class LongestCommonSubstringTest {
+    public static void main(String[] args) {
+        LongestCommonSubstring ys = new LongestCommonSubstring();
+
+        System.out.println(ys.longestCommonSubsequence("qweda","qweda"));
+
+    }
+}
