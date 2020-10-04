137661093f8ece398d0f2050e45ed2bb68c8ff15
H-Always <1790199849@qq.com>
2020-10-04 16:13:14 +0800

    算法测试

--- SimpleAlgorithm/Combine/CombineTest.java
+++ SimpleAlgorithm/Combine/CombineTest.java
@@ -1,17 +1,16 @@
 import java.util.List;
 
 /**
- * @ ClassName : CombineTest
- * @ Description :算法测试
- * @ Author : 余霜
- * @ Date : 2020/09/08/13:19
- * @ Version : 1.0
+ * @ClassName: CombineTest
+ * @Description: 算法测试
+ * @Author: 余霜
+ * @Date: 2020/09/08/13:19
+ * @Version: 1.0
  */
 public class CombineTest {
     public static void main(String[] args) {
         Combine combine = new Combine();
 
-        System.out.println( );
         List<List<Integer>> lists = combine.combine(12,7);
 
         for (List<Integer> list : lists) {
