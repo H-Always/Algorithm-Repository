8eee0db7b4c5d022314471ef94d9f21e3a75451c
H-Always <1790199849@qq.com>
2020-10-04 16:54:02 +0800

    算法测试

---
+++ SimpleAlgorithm/MinimumOperations/MinimumOperationsTest.java
@@ -0,0 +1,20 @@
+/**
+ * @ ClassName : MinimumOperationsTest
+ * @ Description : 算法测试
+ * @ Author : 余霜
+ * @ Date : 2020/09/14 14:39
+ * @ Version : 1.0
+ */
+public class MinimumOperationsTest {
+    public static void main(String[] args) {
+        MinimumOperations minimumOperations = new MinimumOperations();
+
+        String leaves = "rrryyyrryyyrr";
+
+        System.out.println(leaves);
+        System.out.println(minimumOperations.minimumOperations(leaves));
+
+
+
+    }
+}
