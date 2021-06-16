8b7fcd16cb1bc31b20dc1970b9edb02a5ca6048f
H-Always <1790199849@qq.com>
2021-06-16 23:26:45 +0800

    算法测试

--- SimpleAlgorithm/CombinationSum2/CombinationSum2Test.java
+++ SimpleAlgorithm/CombinationSum2/CombinationSum2Test.java
@@ -1,3 +1,4 @@
+import java.math.BigDecimal;
 import java.util.List;
 /**
  * @ ClassName : combinationSum2
@@ -10,15 +11,19 @@ import java.util.List;
 public class CombinationSum2Test {
     public static void main(String[] args) {
 
-        //false代表全部，false代表找一个就行
-        boolean flag = true;
+        //true代表全部，false代表找一个就行
+        boolean flag = false;
 
-        int[] data = new int[]{2,3,3,3,4,5,1,2,3};
-        int target = 8;
+        double[] data = new double[]{10234.58,10001.36,9658.09};
+        double[] buiness = new double[]{20235.94,19659.45,29894.03};
+        double target = 8;
 
-        CombinationSum2 solution = new CombinationSum2();
-        List<List<Integer>> res = solution.combinationSum2(data, target,flag);
-        System.out.println("当前存在匹配数据: " + res);
+        for (double v : buiness) {
+            CombinationSum2 solution = new CombinationSum2();
+            List<List<Double>> res = solution.combinationSum2(data, v, flag );
+            System.out.println(v+"当前存在匹配数据: " + res);
+        }
+
 
     }
 }
