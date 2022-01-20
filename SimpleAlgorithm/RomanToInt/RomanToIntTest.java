50e9d7bb91b2ebf0fbd81e6905cf0c712eb86e94
H-Always <1790199849@qq.com>
2022-01-20 17:13:26 +0800

    罗马数字转整数测试

---
+++ SimpleAlgorithm/RomanToInt/RomanToIntTest.java
@@ -0,0 +1,15 @@
+/**
+ * @ ClassName : RomanToIntTest
+ * @ Description :
+ * @ Author : 余霜
+ * @ Date : 2022/1/20 14:40
+ * @ Version : 1.0
+ **/
+public class RomanToIntTest {
+    public static void main(String[] args) {
+        RomanToInt romanToInt = new RomanToInt();
+
+
+        System.out.println(romanToInt.romanToInt("MCMXCIVIV"));
+    }
+}
